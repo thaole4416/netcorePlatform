ee142f0339ff0dabc4011ee0eb16a186c1076f9f
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 20:18:27 +0700

    Platform: Using Sessions

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,12 @@ namespace Platform
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(opts => { opts.CheckConsentNeeded = context => true; });
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.IsEssential = true;
+            });
         }
 
         public void Configure(IApplicationBuilder app)
@@ -18,17 +24,17 @@ namespace Platform
             app.UseDeveloperExceptionPage();
             app.UseCookiePolicy();
             app.UseMiddleware<ConsentMiddleware>();
+            app.UseSession();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/cookie", async context =>
                 {
-                    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter1", counter1.ToString(),
-                        new CookieOptions {MaxAge = TimeSpan.FromMinutes(30), IsEssential = true});
-                    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter2", counter1.ToString(),
-                        new CookieOptions {MaxAge = TimeSpan.FromMinutes(30)});
+                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+                    context.Session.SetInt32("counter1", counter1);
+                    context.Session.SetInt32("counter2", counter2);
+                    await context.Session.CommitAsync();
                     await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
                 });
                 endpoints.MapGet("clear", context =>
