246f47af568750c2a51d11eccd3fff528be696c9
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 19:55:07 +0700

    Dependency Injection: Creating New Handlers for Each Request

--- Services/EndpointExtensions.cs
+++ Services/EndpointExtensions.cs
@@ -18,14 +18,16 @@ namespace Microsoft.AspNetCore.Builder
                 throw new System.Exception("Method cannot be used");
             }
 
-            T endpointInstance = ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
             ParameterInfo[] methodParams = methodInfo.GetParameters();
-            app.MapGet(path,
-                context => (Task) methodInfo.Invoke(endpointInstance,
+            app.MapGet(path, context =>
+            {
+                T endpointInstance = ActivatorUtilities.CreateInstance<T>(context.RequestServices);
+                return (Task) methodInfo.Invoke(endpointInstance,
                     methodParams.Select(p =>
                         p.ParameterType == typeof(HttpContext)
                             ? context
-                            : context.RequestServices.GetService(p.ParameterType)).ToArray()));
+                            : context.RequestServices.GetService(p.ParameterType)).ToArray());
+            });
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace Platform
             {
                 if (context.Request.Path == "/middleware/function")
                 {
-                    IResponseFormatter formatter = app.ApplicationServices.GetService<IResponseFormatter>();
+                    IResponseFormatter formatter= context.RequestServices.GetService<IResponseFormatter>();
                     await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
                 }
                 else
@@ -38,7 +38,7 @@ namespace Platform
                 endpoints.MapGet("/endpoint/function",
                     async context =>
                     {
-                        IResponseFormatter formatter = app.ApplicationServices.GetService<IResponseFormatter>();
+                        IResponseFormatter formatter= context.RequestServices.GetService<IResponseFormatter>();
                         await formatter.Format(context, "Endpoint Function: It is sunny in LA");
                     });
             });
