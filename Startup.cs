afdf0dff0411e757c181b94efd03efdccb40093a
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 19:17:36 +0700

    URL Routing: Accessing the Endpoint in a Middleware Component

--- Startup.cs
+++ Startup.cs
@@ -20,14 +20,30 @@ namespace Platform
         {
             app.UseDeveloperExceptionPage();
             app.UseRouting();
+            app.Use(async (context, next) =>
+            {
+                Endpoint end = context.GetEndpoint();
+                if (end != null)
+                {
+                    await context.Response.WriteAsync($"{end.DisplayName} Selected \n");
+                }
+                else
+                {
+                    await context.Response.WriteAsync("No Endpoint Selected \n");
+                }
+
+                await next();
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.Map("{number:int}",
                         async context => { await context.Response.WriteAsync("Routed to the int endpoint"); })
+                    .WithDisplayName("Int Endpoint")
                     .Add(b => ((RouteEndpointBuilder) b).Order = 1);
                 ;
                 endpoints.Map("{number:double}",
                         async context => { await context.Response.WriteAsync("Routed to the double endpoint"); })
+                    .WithDisplayName("Double Endpoint")
                     .Add(b => ((RouteEndpointBuilder) b).Order = 2);
                 ;
             });
