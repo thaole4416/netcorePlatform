43b4bbc42606047fd33095a4cbdbe5b0b615434b
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 20:39:34 +0700

    Platform: Handling Exceptions and Errors

--- Startup.cs
+++ Startup.cs
@@ -19,43 +19,28 @@ namespace Platform
                 options.IdleTimeout = TimeSpan.FromMinutes(30);
                 options.Cookie.IsEssential = true;
             });
-            services.AddHsts(opts => {opts.MaxAge = TimeSpan.FromDays(1);opts.IncludeSubDomains = true;});
+            services.AddHsts(opts =>
+            {
+                opts.MaxAge = TimeSpan.FromDays(1);
+                opts.IncludeSubDomains = true;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsProduction()) {app.UseHsts();} 
-            app.UseDeveloperExceptionPage();
+            //app.UseDeveloperExceptionPage();
+app.UseExceptionHandler("/error.html");
+            if (env.IsProduction())
+            {
+                app.UseHsts();
+            }
+
             app.UseHttpsRedirection();
             app.UseCookiePolicy();
+            app.UseStaticFiles();
             app.UseMiddleware<ConsentMiddleware>();
             app.UseSession();
-            app.UseRouting();
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
-                await next();
-            });
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/cookie", async context =>
-                {
-                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-                    context.Session.SetInt32("counter1", counter1);
-                    context.Session.SetInt32("counter2", counter2);
-                    await context.Session.CommitAsync();
-                    await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-                });
-                endpoints.MapGet("clear", context =>
-                {
-                    context.Response.Cookies.Delete("counter1");
-                    context.Response.Cookies.Delete("counter2");
-                    context.Response.Redirect("/");
-                    return Task.CompletedTask;
-                });
-                endpoints.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
-            });
+            app.Run(context => { throw new Exception("Something has gone wrong"); });
         }
     }
 }
