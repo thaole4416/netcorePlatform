04c0501e3d3c762f45fb7f4ce9d2f3d8e1cfe858
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 20:14:13 +0700

    Platform: Using  Cookies

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +18,23 @@ namespace Platform
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("/cookie", async context =>
+                {
+                    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+                    context.Response.Cookies.Append("counter1", counter1.ToString(),
+                        new CookieOptions {MaxAge = TimeSpan.FromMinutes(30)});
+                    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+                    context.Response.Cookies.Append("counter2", counter1.ToString(),
+                        new CookieOptions {MaxAge = TimeSpan.FromMinutes(30)});
+                    await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+                });
+                endpoints.MapGet("clear", context =>
+                {
+                    context.Response.Cookies.Delete("counter1");
+                    context.Response.Cookies.Delete("counter2");
+                    context.Response.Redirect("/");
+                    return Task.CompletedTask;
+                });
                 endpoints.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
             });
         }
