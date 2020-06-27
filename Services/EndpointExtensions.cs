caa6404d34e580b278b377470300f154ba82784a
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 19:41:23 +0700

    Dependency Injection: Using a Service in an Endpoint with the Activation Utility Class

--- Services/EndpointExtensions.cs
+++ Services/EndpointExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Routing;
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Platform.Services;
 
@@ -6,10 +9,16 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static class EndpointExtensions
     {
-        public static void MapWeather(this IEndpointRouteBuilder app, string path)
+        public static void MapEndpoint<T>(this IEndpointRouteBuilder app, string path, string methodName = "Endpoint")
         {
-            IResponseFormatter formatter = app.ServiceProvider.GetService<IResponseFormatter>();
-            app.MapGet(path, context => Platform.WeatherEndpoint.Endpoint(context, formatter));
+            MethodInfo methodInfo = typeof(T).GetMethod(methodName);
+            if (methodInfo == null || methodInfo.ReturnType != typeof(Task))
+            {
+                throw new System.Exception("Method cannot be used");
+            }
+
+            T endpointInstance = ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
+            app.MapGet(path, (RequestDelegate) methodInfo.CreateDelegate(typeof(RequestDelegate), endpointInstance));
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace Platform
             app.UseEndpoints(endpoints =>
             {
                 //endpoints.MapGet("/endpoint/class", WeatherEndpoint.Endpoint);
-                endpoints.MapWeather("/endpoint/class");
+                endpoints.MapEndpoint<WeatherEndpoint>("/endpoint/class");
                 endpoints.MapGet("/endpoint/function",
                     async context => { await formatter.Format(context, "Endpoint Function: It is sunny in LA"); });
             });
