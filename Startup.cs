cedf68ede9977a140eb7a44fee0a0b62611042e1
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 20:12:33 +0700

    Platform: Using Static Content and Client-Side Packages

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace Platform
@@ -34,6 +35,11 @@ namespace Platform
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"), RequestPath = "/files"
+            });
             app.UseRouting();
             app.UseMiddleware<LocationMiddleware>();
             app.UseEndpoints(endpoints =>
