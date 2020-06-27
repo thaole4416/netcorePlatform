c7ab178dcae1a4b9eae74bb4832da1314dadddf7
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 20:41:04 +0700

    Platform: Enriching Status Code Responses

--- Startup.cs
+++ Startup.cs
@@ -29,17 +29,30 @@ namespace Platform
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             //app.UseDeveloperExceptionPage();
-app.UseExceptionHandler("/error.html");
+            app.UseExceptionHandler("/error.html");
             if (env.IsProduction())
             {
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStatusCodePages("text/html", Responses.DefaultResponse);
             app.UseCookiePolicy();
             app.UseStaticFiles();
             app.UseMiddleware<ConsentMiddleware>();
             app.UseSession();
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/error")
+                {
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    await Task.CompletedTask;
+                }
+                else
+                {
+                    await next();
+                }
+            });
             app.Run(context => { throw new Exception("Something has gone wrong"); });
         }
     }
