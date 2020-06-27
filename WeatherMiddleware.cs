e3f9e6c36e1b1053fd8c59b6c4ff79b3185f70f9
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 19:31:11 +0700

    Dependency Injection: Using a Service in a Middleware Class

--- WeatherMiddleware.cs
+++ WeatherMiddleware.cs
@@ -1,22 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using Platform.Services;
 
 namespace Platform
 {
     public class WeatherMiddleware
     {
         private RequestDelegate next;
+        private IResponseFormatter formatter;
 
-        public WeatherMiddleware(RequestDelegate nextDelegate)
+        public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter respFormatter)
         {
             next = nextDelegate;
+            formatter = respFormatter;
         }
 
         public async Task Invoke(HttpContext context)
         {
             if (context.Request.Path == "/middleware/class")
             {
-                await context.Response.WriteAsync("Middleware Class: It is raining in London");
+                await formatter.Format(context, "Middleware Class: It is raining in London");
             }
             else
             {
