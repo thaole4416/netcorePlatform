d1a19bdf49e4114f0d204d030cc640b8934373c5
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 18:41:22 +0700

    URL Routing: Use routing with middleware class

--- Startup.cs
+++ Startup.cs
@@ -14,8 +14,20 @@ namespace Platform
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
-            app.UseMiddleware<Population>();
-            app.UseMiddleware<Capital>();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("{first}/{second}/{third}", async context =>
+                {
+                    await context.Response.WriteAsync("Request Was Routed\n");
+                    foreach (var kvp in context.Request.RouteValues)
+                    {
+                        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+                    }
+                });
+                endpoints.MapGet("capital/uk", new Capital().Invoke);
+                endpoints.MapGet("population/paris", new Population().Invoke);
+            });
             app.Use(async (context, next) => { await context.Response.WriteAsync("Terminal Middleware Reached"); });
         }
     }
