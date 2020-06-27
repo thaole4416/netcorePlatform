7680ea791ddd615a16fd47c7d6ac1f53252b524b
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 20:05:36 +0700

    Platform: User Secrets

--- Startup.cs
+++ Startup.cs
@@ -35,6 +35,12 @@ namespace Platform
             {
                 string defaultDebug = Configuration["Logging:LogLevel:Default"];
                 await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
+                string environ = Configuration["ASPNETCORE_ENVIRONMENT"];
+                await context.Response.WriteAsync($"\nThe env setting is: {environ}");
+                string wsID = Configuration["WebService:Id"];
+                string wsKey = Configuration["WebService:Key"];
+                await context.Response.WriteAsync($"\nThe secret ID is: {wsID}");
+                await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}");
             });
             app.UseEndpoints(endpoints =>
             {
