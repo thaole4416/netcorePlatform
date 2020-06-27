1ba2ac2fe15cea9ee3ba04a3f516c31ff9b5f64c
ThaoLe98 <thaole4416@gmail.com>
2020-06-27 20:01:22 +0700

    Platform: Accessing Configuration Settings

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
@@ -17,10 +18,15 @@ namespace Platform
         {
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration config)
         {
             app.UseDeveloperExceptionPage();
             app.UseRouting();
+            app.Use(async (context, next) =>
+            {
+                string defaultDebug = config["Logging:LogLevel:Default"];
+                await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
